4437c206234cc12a9b4eadcea99971943a12db38
cnsr <dmytrenkoyar@gmail.com>
2019-11-09 20:08:08 +0200

    started working on frontend

--- reeddit/src/App.tsx
+++ reeddit/src/App.tsx
@@ -1,29 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { types } from "mobx-state-tree"
+import { types } from "mobx-state-tree";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
 
+import { Menu } from 'semantic-ui-react';
 
+import 'semantic-ui-css/semantic.min.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">			
+				<Menu>
+					<Menu.Item as={Link} to="/">Home</Menu.Item>
+					<Menu.Item as={Link} to="/about">About</Menu.Item>
+					<Menu.Item as={Link} to="/users">Users</Menu.Item>
+				</Menu>
+			{/* A <Switch> looks through its children <Route>s and
+			renders the first one that matches the current URL. */}
+			<Switch>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/users">
+					<Users />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+			</div>		
+		</Router>
+);
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
